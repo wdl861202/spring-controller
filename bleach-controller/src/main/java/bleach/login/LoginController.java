e1416a9a74d63ed9be6f2d88d709fa8faaaa5ebd
wangdl <wdl861202@163.com>
2020-06-03 18:12:38 +0800

    feat:基础工程

--- bleach-controller/src/main/java/bleach/login/LoginController.java
+++ bleach-controller/src/main/java/bleach/login/LoginController.java
@@ -1,8 +1,15 @@
 package bleach.login;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
 
+	@RequestMapping(method= {RequestMethod.GET,RequestMethod.POST},path= {"/login1"},produces = { "application/json;charset=UTF-8" ,"text/plain;charset=UTF-8"})
+	public String login() {
+		return "SUCCESS";
+	}
+	
 }
--- bleach-service/src/main/java/bleach/login/LoginImpl.java
+++ bleach-service/src/main/java/bleach/login/LoginImpl.java
@@ -1,6 +1,5 @@
 package bleach.login;
 
-import javax.annotation.PostConstruct;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,13 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 import main.java.login.ILogin;
 
 @RestController
-public class LoginImpl  {
+public class LoginImpl  implements ILogin{
 
-	@PostConstruct
-	public void init() {
-		String a="";
-	}
-	
 	@RequestMapping(method= {RequestMethod.GET,RequestMethod.POST},path= {"/login"},produces = { "application/json;charset=UTF-8" ,"text/plain;charset=UTF-8"})
 	public String login(String name, String password) {
 		return "SUCCESS";
