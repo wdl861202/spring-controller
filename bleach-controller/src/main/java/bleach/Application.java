4043dd65222c44fd7f9ba106f315b7d8806f6633
wangdl <wdl861202@163.com>
2020-06-04 18:08:44 +0800

    feat:增加parent

--- bleach-controller/src/main/java/bleach/Application.java
+++ bleach-controller/src/main/java/bleach/Application.java
@@ -1,13 +1,14 @@
+
 package bleach;
+
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@ImportResource({"classpath:bleach-application.xml"})
+@ImportResource({ "classpath:bleach-application.xml" })
 public class Application {
 
 	public static void main(String[] args) {
--- bleach-controller/src/main/java/bleach/login/LoginController.java
+++ bleach-controller/src/main/java/bleach/login/LoginController.java
@@ -1,4 +1,8 @@
+
 package bleach.login;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoginController {
 
-	@RequestMapping(method= {RequestMethod.GET,RequestMethod.POST},path= {"/login1"},produces = { "application/json;charset=UTF-8" ,"text/plain;charset=UTF-8"})
-	public String login() {
-		return "SUCCESS";
+	@RequestMapping(method = { RequestMethod.GET, RequestMethod.POST }, path = { "/login1" }, produces = { "application/json;charset=UTF-8", "text/plain;charset=UTF-8" })
+	public Map<String, String> login() {
+		Map<String, String> m = new HashMap<String, String>();
+		m.put("RESULT", "success");
+		return m;
 	}
-	
+
 }
